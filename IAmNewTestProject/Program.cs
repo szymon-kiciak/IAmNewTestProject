106b71af0eca26c4c7fce7d1e0892b36ddc361c5
s k <szymon.kiciak@gmail.com>
2017-11-01 21:57:12 +0000

    Commit 20171101
    Added little thingsxczczxcxzc

--- IAmNewTestProject/Program.cs
+++ IAmNewTestProject/Program.cs
@@ -13,6 +13,9 @@ namespace IAmNewTestProject
             Console.WriteLine("And this is new stuff");
             Console.WriteLine("We added extra line in the new branch");
             Console.ReadKey();
+
+            Console.Write("We make new changes into newbranchtest");
+            Console.Write("20171101");
         }
     }
 }
